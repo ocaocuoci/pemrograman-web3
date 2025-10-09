71bc5aac84494b3b9c7dda3ff4324b0faa489082
ocaocuoci <hernandar0606@gmail.com>
2025-10-09 09:40:15 +0700

    prak7 2

--- JS07_PHP-FORM/form_validasi.php
+++ JS07_PHP-FORM/form_validasi.php
@@ -2,21 +2,49 @@
 <html>
 <head>
     <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <h1>Form Input dengan Validasi</h1>
-    <form action="proses_validasi.php" method="post">
+    <form id="myForm" action="proses_validasi.php" method="post">
         <label for="nama">Nama: </label>
         <input type="text" id="nama" name="nama">
-        <br>
-        <br>
+        <span id="nama-error" style="color: red;"></span><br><br>
 
         <label for="email">Email: </label>
         <input type="text" id="email" name="email">
-        <br>
-        <br>
-        
+        <span id="email-error" style="color: red;"></span><br><br>
+
         <input type="submit" value="Submit">
     </form>
+
+    <script>
+        $(document).ready(function(){
+            $("#myForm").submit(function(event){
+                var nama = $("#nama").val();
+                var email = $("#email").val();
+                var valid = true;
+
+                
+                if (nama == ""){
+                    $("#nama-error").text("Nama harus diisi.");
+                    valid = false;
+                } else {
+                    $("#nama-error").text("");
+                }
+
+                if (email == ""){
+                    $("#email-error").text("Email harus diisi.");
+                    valid = false;
+                } else {
+                    $("#email-error").text("");
+                }
+               
+                if (!valid) {
+                    event.preventDefault();
+                }
+            });
+        });
+    </script>
 </body>
 </html>
