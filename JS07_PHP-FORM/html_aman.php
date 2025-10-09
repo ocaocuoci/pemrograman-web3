27b7f522bd677a1877cfb2b247d09d2c75bb8f1d
ocaocuoci <hernandar0606@gmail.com>
2025-10-09 07:55:19 +0700

    prak4 1

--- JS07_PHP-FORM/html_aman.php
+++ JS07_PHP-FORM/html_aman.php
@@ -1,34 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>HTML Aman & Validasi Email</title>
+    <title>HTML_Aman</title>
 </head>
 <body>
-    <h2>Form Input & Validasi Email</h2>
+    <h2>Form Input</h2>
     <form method="post" action="">
         <label>Masukkan Nama:</label>
-        <input type="text" name="nama"><br><br>
-
-        <label>Masukkan Email:</label>
-        <input type="text" name="email"><br><br>
-
+        <input type="text" name="nama">
         <input type="submit" value="Kirim">
     </form>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Bagian input aman
+        
         $input = $_POST['nama'];
+       
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-        echo "<p>Hasil input nama: $input</p>";
 
-        // Bagian validasi email
-        $email = $_POST['email'];
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "<p>Email valid: $email</p>";
-        } else {
-            echo "<p>Email tidak valid!</p>";
-        }
+        echo "<p>Hasil input nama: $input</p>";
     }
     ?>
 </body>
